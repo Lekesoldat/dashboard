d46626b2f79142ba412b5da7d52451e1738ac63f
Magnus L. Holtet <magnulho@gmail.com>
2019-07-09 20:32:08 +0200

    Correct typo.

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
+const GlobalStyles = createGlobalStyle`
   @font-face {
     font-family: 'Roboto', sans-serif;
     src: url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
